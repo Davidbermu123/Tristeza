e3f9fb2b0067e0231daa9c2d2b34d052be666982
Davidbermu123 <david.bermudez01@usa.edu.co>
2024-09-01 22:30:39 -0500

    ultimo de hoy

--- Ingenieria/Ingenieria/Ingenieria/src/main/java/Trabajo/Ingenieria/Controladores/AnalisisControlador.java
+++ Ingenieria/Ingenieria/Ingenieria/src/main/java/Trabajo/Ingenieria/Controladores/AnalisisControlador.java
@@ -1,13 +1,14 @@
 package Trabajo.Ingenieria.Controladores;
 
 import Trabajo.Ingenieria.Entidades.EntidadAnalisis;
+import Trabajo.Ingenieria.Entidades.usuario;
 import Trabajo.Ingenieria.Servicios.AnalisisComprasService;
 import Trabajo.Ingenieria.Servicios.AnalisisVentasService;
+import Trabajo.Ingenieria.Servicios.clienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -24,8 +25,34 @@ public class AnalisisControlador {
     @Autowired
     private AnalisisComprasService comprasService;
 
+    @Autowired
+    private clienteServicio usuarioService;
+
+    @GetMapping("/rol")
+    public String obtenerRolPorUsuario() {
+        // Obtener el nombre de usuario del objeto Authentication
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        // Buscar el usuario en el servicio Usuario_modelos
+        usuario user = usuarioService.findByUsername(username);
+
+        // Verificar si el usuario existe y devolver su rol
+        if (user != null) {
+            return user.getRol().toString();
+        } else {
+            return "Usuario no encontrado";
+        }
+    }
+
     @GetMapping("/ventas-semanal")
     public List<Map<String, Object>> obtenerVentasDiarias(@RequestParam("inicio") String inicio, @RequestParam("fin") String fin) {
+        // Verificar el rol del usuario antes de procesar la solicitud
+        String rol = obtenerRolPorUsuario();
+        if (!"ADMIN".equals(rol) && !"VENTAS".equals(rol)) {
+            throw new SecurityException("No tienes permiso para acceder a esta información");
+        }
+
         List<EntidadAnalisis> ventasDiarias = ventasService.obtenerVentasPorFecha(inicio, fin);
         List<Map<String, Object>> ventasPorFecha = new ArrayList<>();
         
@@ -41,6 +68,12 @@ public class AnalisisControlador {
     
     @GetMapping("/compras-semanal")
     public List<Map<String, Object>> obtenerComprasDiarias(@RequestParam("inicio") String inicio, @RequestParam("fin") String fin) {
+        // Verificar el rol del usuario antes de procesar la solicitud
+        String rol = obtenerRolPorUsuario();
+        if (!"ADMIN".equals(rol) && !"COMPRAS".equals(rol)) {
+            throw new SecurityException("No tienes permiso para acceder a esta información");
+        }
+
         List<EntidadAnalisis> comprasDiarias = comprasService.obtenerComprasPorFecha(inicio, fin);
         List<Map<String, Object>> comprasPorFecha = new ArrayList<>();
     
